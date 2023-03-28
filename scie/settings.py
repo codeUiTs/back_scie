ccafd9c3c5c4c76df1eb4610b6326a09373f0be4
Miszaell <misaelvgm011@gmail.com>
2023-03-28 15:52:55 -0600

    F9

--- scie/settings.py
+++ scie/settings.py
@@ -119,15 +119,15 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'scie.wsgi.application'
 
-if 'RDS_DB_NAME' in os.environ:
+if 'DB_HOST' in os.environ:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.postgresql_psycopg2',
-            'NAME': os.environ['RDS_DB_NAME'],
-            'USER': os.environ['RDS_USERNAME'],
-            'PASSWORD': os.environ['RDS_PASSWORD'],
-            'HOST': os.environ['RDS_HOSTNAME'],
-            'PORT': os.environ['RDS_PORT'],
+            'NAME': os.environ['DB_NAME'],
+            'USER': os.environ['DB_USERNAME'],
+            'PASSWORD': os.environ['DB_PASSWORD'],
+            'HOST': os.environ['DB_HOST'],
+            'PORT': os.environ['DB_PORT'],
         }
     }
 else:
